35257bb39c24bfb5985f7f0efefae9874ce86cf3
Daniella Toth <daniellatoth@me.com>
2026-08-18 15:05:18 -0400

    merged old version of portfolio site for a simple view toggle

---
+++ root-cool_site/toggle.js
@@ -0,0 +1,12 @@
+(function () {
+    const checkbox = document.getElementById('viewToggle');
+    if (!checkbox){
+        return;
+    } 
+  
+    checkbox.addEventListener('change', function () {
+      if (checkbox.checked) {
+        window.location.href = '../simple/index.html';
+      }
+    });
+  })();
